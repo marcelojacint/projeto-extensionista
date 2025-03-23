ed2537db1d83b963c29ff9485346306701fd5b50
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-03-23 14:55:44 -0300

    finalizado cadastro e login simples no js

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,12 +1,13 @@
 function cadastrar() {
     let nome = document.getElementById("nome").value;
-    let email = document.getElementById("email").value;
+    let email = document.getElementById("email").value.trim().toLowerCase();
     let telefone = document.getElementById("telefone").value;
     let senha = document.getElementById("senha").value;
 
     if (nome && email && telefone && senha) {
         let usuario = {nome, email, telefone, senha};
         localStorage.setItem(email, JSON.stringify(usuario));
+        console.log(localStorage.getItem(email))
         alert("Cadastro realizado com sucesso!")
         window.location.href = "login.html";
     } else{
